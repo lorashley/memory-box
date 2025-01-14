15c3e98352b37f92effda16540d5b1640a8817fe
Lauren Ramgattie <lauren@withdouble.com>
2025-01-13 21:22:48 -0500

    ✨ Enhance FavoriteHeart component with hover effect and click handler for improved interactivity

--- src/components/FavoriteHeart.tsx
+++ src/components/FavoriteHeart.tsx
@@ -1,16 +1,32 @@
 import FavoriteIcon from '@mui/icons-material/Favorite'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import pink from '@mui/material/colors/pink'
+import useSwitch from '../hooks/useSwitch'
 
 type Props = {
   isFavorite?: boolean
+  onClick?: () => void
 }
 
-const FavoriteHeart = ({ isFavorite }: Props) => {
-  return isFavorite ? (
-    <FavoriteIcon sx={{ color: pink[500] }} />
-  ) : (
-    <FavoriteBorderIcon sx={{ color: pink[500] }} />
+const FavoriteHeart = ({ isFavorite, onClick }: Props) => {
+  const [isHovered, setIsHovered, unsetIsHovered] = useSwitch(false)
+  return (
+    <div
+      onClick={onClick}
+      onMouseEnter={setIsHovered}
+      onMouseLeave={unsetIsHovered}
+    >
+      {isFavorite || isHovered ? (
+        <FavoriteIcon sx={{ color: pink[500] }} onClick={onClick} />
+      ) : (
+        <FavoriteBorderIcon
+          sx={{
+            color: pink[500],
+          }}
+          onClick={onClick}
+        />
+      )}
+    </div>
   )
 }
 
